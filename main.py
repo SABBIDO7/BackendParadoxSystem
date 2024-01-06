2bcfc37e37cadb604bd4263dd15db738ec8036a0
yarachidiac <88508520+yarachidiac@users.noreply.github.com>
2024-01-06 14:13:52 +0200

    PoS and Team commit

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from fastapi.middleware.cors import CORSMiddleware
-import hashlib
-from sqlalchemy import create_engine,func,text
-from sqlalchemy.orm import sessionmaker
-import models
-from fastapi import FastAPI, HTTPException,status,Request
-from sqlalchemy.ext.declarative import declarative_base
+import json
 
+from fastapi import FastAPI, HTTPException, Request
+import mysql.connector
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -17,79 +14,273 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/AddCompany/",status_code=status.HTTP_201_CREATED)
-async def add_company(request:Request):
+DATABASE_CONFIG = {
+    "user": "root",
+    "password": "yara",
+    "host": "localhost",
+    "port": 3308,
+}
+
+def get_db(company_name: str):
+    try:
+        print("companyyyyyyyyyyyy nameeee",company_name)
+        # Connect to the database using MySQL Connector/Python
+        connection = mysql.connector.connect(
+            database=company_name,
+            **DATABASE_CONFIG
+        )
+        return connection
+    except mysql.connector.Error as err:
+        if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
+            raise HTTPException(status_code=401, detail="Invalid credentials")
+        elif err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+            raise HTTPException(status_code=404, detail="Company not found")
+        else:
+            raise HTTPException(status_code=500, detail="Internal Server Error")
+
+@app.post("/login")
+async def login(request: Request):
     try:
         data = await request.json()
+        username = data.get('username')
+        password = data.get('password')
+        company_name = data.get('company_name')
 
-        engine=create_engine(f'mysql+pymysql://root:root@localhost:3307')
-        SessionLocal= sessionmaker(autocommit=False, autoflush= False, bind=engine)
-        Base = declarative_base()
+        user_query = (
+            f"SELECT * FROM users "
+            f"WHERE username = '{username}' AND password = '{password}' "
+            f"AND EXISTS (SELECT name FROM company WHERE name = '{company_name}')"
+        )
 
+        # Establish the database connection
+        conn = get_db(company_name)
 
-        db = SessionLocal()
+        cursor = conn.cursor()
+        cursor.execute(user_query)
+        user = cursor.fetchone()
 
-        query = text(f"SELECT SCHEMA_NAME FROM      INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{data.CompanyName}'")
-        result = db.execute(query).fetchone()
+        if not user:
+            raise HTTPException(status_code=401, detail="Invalid credentials")
 
-        if result:
-            # Database with the same name already exists
-            raise HTTPException(status_code=400, detail="Database with this name already exists")
-
-        # Create a new database and insert the company information
-        create_db_query = text(f"CREATE DATABASE {data.CompanyName}")
-        db.execute(create_db_query)
-        if db:
-                    engine2=create_engine(f'mysql+pymysql://root:root@localhost:3307/{data.CompanyName}')
-        SessionLocal2= sessionmaker(autocommit=False, autoflush= False, bind=engine2)
-
-        # Create tables in the new database
-        Base.metadata.create_all(bind=engine2)
-        db2 = SessionLocal2()
-        post_companyDetails = models.CompanyManagementP(name=data.CompanyName,tel=data.tel,currency=data.currency)
-        db2.add(post_companyDetails)
-        db2.commit()
-        if db2:
-            
-            create_dept=models.DepartmentsP(name=data.departmentName,description=data.departmentDescription)
-            db2.add(create_dept)
-            db2.commit()
-            post_user=models.UsersP(name=data.username,usercode=data.usercode,department_id=create_dept.id)
-
-            db2.add(post_user)
-            db2.commit()
-
-            return {"status": True, "message": "Company added successfully"}
-    except Exception as e:
-        return {"status": False, "error": str(e)}
+        print("Received data:", username, password, company_name)
+        print("Authentication successful:", username, company_name)
+        return {"message": "Login successful", "user": user}
+    except HTTPException as e:
+        print("Validation error details:", e.detail)
+        raise e
     finally:
-        db.close()
+        # The connection will be automatically closed when it goes out of scope
+        pass
 
-
-
-
-
-@app.post("/Checkuser/",status_code=status.HTTP_201_CREATED)
-async def authenticate_user(username, password,CompanyName):
+@app.get("/users/{company_name}")
+async def get_users(company_name: str):
     try:
-        engine=create_engine(f'mysql+pymysql://root:root@localhost:3307/{CompanyName}')
-        SessionLocal= sessionmaker(autocommit=False, autoflush= False, bind=engine)
+        # Establish the database connection
+        conn = get_db(company_name)
+        cursor = conn.cursor()
+        user_query = (
+            f"SELECT * FROM users "
+            f"WHERE EXISTS (SELECT name FROM company WHERE name = '{company_name}')"
+        )
 
-        db = SessionLocal()
-    
-        user = db.query(models.Users).filter(models.Users.username == username).first()
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        print(hashed_password)
-        print(user.username)
-        print(user.password)
-        # Check if the user exists and the provide
-        # d password matches the stored hashed password
-        if user and user.password==hashed_password:
-            return {"status":True}  # Authentication successful
-        else:
-            return {"status":False}  # Authentication failed
-    except Exception as e:
-        # Handle the error when the database doesn't exist
-        return {"status": False, "error": "Database does not exist or connection failed"}
+        cursor.execute(user_query)
+        users = cursor.fetchall()
+
+        # Get column names from cursor.description
+        column_names = [desc[0] for desc in cursor.description]
+
+        # Convert the list of tuples to a list of dictionaries
+        users_list = [dict(zip(column_names, user)) for user in users]
+
+        # print("userssssssssssssssssssssssssssssss", users_list)
+
+        return users_list
+    except HTTPException as e:
+        print("Error details:", e.detail)
+        raise e
     finally:
-        db.close()
+        # The connection will be automatically closed when it goes out of scope
+        pass
+
+
+@app.get("/getUserDetail/{company_name}/{username}")
+async def get_user_detail(company_name: str, username: str):
+    try:
+        # Establish the database connection
+        conn = get_db(company_name)
+        cursor = conn.cursor()
+        user_query = "SELECT * FROM users WHERE username=%s"
+        cursor.execute(user_query, (username,))
+        user = cursor.fetchone()
+
+        print("userssssssssssssssssssssssssssssss", user)
+        # Get column names from cursor.description
+
+        # Convert the tuple to a dictionary
+        user_dict = dict(zip(cursor.column_names, user))
+
+        return user_dict
+    except HTTPException as e:
+        print("Error details:", e.detail)
+        raise e
+    finally:
+        # The connection will be automatically closed when it goes out of scope
+        pass
+
+
+
+@app.post("/users/{company_name}/{user_id}")
+async def update_user(
+        company_name: str,
+        user_id: int,
+        request: Request,
+):
+    conn = None
+    try:
+        # Check if the user exists in the given company
+        conn = get_db(company_name)
+        cursor = conn.cursor()
+
+        # Check if the user exists
+        user_query = "SELECT * FROM users WHERE id = %s"
+        cursor.execute(user_query, (user_id,))
+        user = cursor.fetchone()
+
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        # Get JSON data from request body
+        data = await request.json()
+        print("dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", data)
+
+        # Convert username to uppercase if it exists in the data
+        if 'username' in data:
+            data['username'] = data['username'].upper()
+            print("ana bl usernameeeeeeeeeeeeeeee", data['username'])
+
+        # Construct the SQL update query
+        update_query = f"UPDATE users SET {', '.join(f'{field} = %s' for field in data)} WHERE id = %s"
+        update_values = list(data.values())
+        print("updateddddddddddddddddddddddd valuessssssssssssssss", update_values)
+        update_values.append(user_id)
+
+        # Execute the update query
+        cursor.execute(update_query, tuple(update_values))
+
+        # Commit the changes to the database
+        conn.commit()
+
+        return {"message": "User details updated successfully", "user": user}
+    except HTTPException as e:
+        print("Error details:", e.detail)
+        raise e
+    finally:
+        if conn:
+            conn.close()
+
+
+@app.get("/company/{company_name}")
+async def get_company(company_name: str):
+    try:
+        # Establish the database connection
+        conn = get_db(company_name)
+        cursor = conn.cursor()
+        user_query = (
+            f"SELECT * FROM company "
+
+        )
+
+        cursor.execute(user_query)
+        users = cursor.fetchall()
+
+        # Get column names from cursor.description
+        column_names = [desc[0] for desc in cursor.description]
+
+        # Convert the list of tuples to a list of dictionaries
+        users_list = [dict(zip(column_names, user)) for user in users]
+
+        print("userssssssssssssssssssssssssssssss", users_list)
+
+        return users_list
+    except HTTPException as e:
+        print("Error details:", e.detail)
+        raise e
+    finally:
+        # The connection will be automatically closed when it goes out of scope
+        pass
+
+
+@app.post("/addusers/{company_name}/{user_name}")
+async def add_user(
+        company_name: str,
+        user_name: str,
+        request: Request,
+):
+    conn = None
+    try:
+        # Check if the user exists in the given company
+        conn = get_db(company_name)
+        cursor = conn.cursor()
+
+        # Check if the user exists
+        user_query = f"SELECT * FROM users WHERE username = %s"
+
+        cursor.execute(user_query, (user_name,))
+
+        user = cursor.fetchone()
+        # user_dict = dict(zip(cursor.column_names, user))
+        # print("dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", user_dict)
+        print("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",user)
+        if user is not None:
+            return {"message": "User already exists"}
+
+        # Convert username to uppercase
+        user_name_uppercase = user_name.upper()
+
+        # Get JSON data from request body
+        data = await request.json()
+        print("dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", data)
+
+        # Perform the actual insert operation
+        insert_query = f"INSERT INTO users(username, password, user_control, email, sales, sales_return, purshase, purshase_return, orders, trans, items, chart, statement) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(insert_query, (user_name_uppercase, '', '', '', '', '', '', '', '', '', '', '', ''))
+
+        # Commit the changes to the database
+        conn.commit()
+
+        return {"message": "User added successfully", "user": user_name_uppercase}
+    except HTTPException as e:
+        print("Error details:", e.detail)
+        raise e
+    finally:
+        if conn:
+            conn.close()
+
+@app.get("/categories/{company_name}")
+async def get_categories(company_name: str):
+    try:
+        # Establish the database connection
+        conn = get_db(company_name)
+        cursor = conn.cursor()
+        category_query = (
+            f"SELECT * FROM groupitem"
+        )
+
+        cursor.execute(category_query)
+        categories = cursor.fetchall()
+
+        # Get column names from cursor.description
+        column_names = [desc[0] for desc in cursor.description]
+
+        # Convert the list of tuples to a list of dictionaries
+        categories_list = [dict(zip(column_names, category)) for category in categories]
+
+        print("hol l categoriesssssss", categories_list)
+
+        return categories_list
+    except HTTPException as e:
+        print("Error details:", e.detail)
+        raise e
+    finally:
+        # The connection will be automatically closed when it goes out of scope
+        pass
